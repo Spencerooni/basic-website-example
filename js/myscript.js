38b352ec286b8a646a6e4b20d4e708a70d9cd453
David Spence <davids@vinnie.local>
2015-10-09 16:57:28 +0100

    Adding search function

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,25 @@
 
-function somethingIsChanging()
+function somethingIsChanging(theSomething)
 {
+	console.log(theSomething);
+}
+
+function filterList()
+{
+	var textToSearchFor = document.getElementById('search').value;
+	var peopleList = document.getElementById('people-list');
+	var people = peopleList.getElementsByTagName('li');
+
+	for (var i = 0; i < people.length; i++)
+	{
+		if (people[i].innerHTML.toLowerCase().indexOf(textToSearchFor.toLowerCase()) > -1)
+		{
+			people[i].style.display = '';
+		}
+		else
+		{
+			people[i].style.display = 'none';
+		}
+
+	}
 }
